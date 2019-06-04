3c0594de29846ec6d6780db13ccd2447720de326
JeongHo16 <31890732+JeongHo16@users.noreply.github.com>
2019-06-04 16:31:19 +0900

    Initial commit

---
+++ Assets/_Scripts/Clicker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clicker
+{
+    public bool clicked()
+    {
+        return Input.anyKeyDown;
+    }
+}
+
+//    {
+//#if (UNITY_ANDROID || UNITY_IPHONE)
+//        return Cardboard.SDK.CardboardTriggered;
+//#else
+//        return Input.anyKeyDown;
+//#endif
+//    }
