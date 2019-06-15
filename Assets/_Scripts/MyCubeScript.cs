d66f6bddf7759554e27cdb363126df36c9bcd980
JeongHo16 <31890732+JeongHo16@users.noreply.github.com>
2019-06-15 23:45:20 +0900

    Add RedSection

---
+++ Assets/_Scripts/MyCubeScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MyCubeScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		print("Pointer In");
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		print("Pointer Out");
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/_Scripts/Trophy.cs
+++ Assets/_Scripts/Trophy.cs
@@ -4,8 +4,27 @@ using UnityEngine;
 
 public class Trophy : MonoBehaviour
 {
+    float velocity = 1f;
+
     private void Update()
     {
-        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        //transform.Rotate(new Vector3(0, 180, 0) * Time.deltaTime);
+        float y = transform.position.y;
+        y += velocity * Time.deltaTime;
+
+        if (y > 1f)
+        {
+            y = 1f;
+            velocity = -velocity;
+        }
+        if (y < 0f)
+        {
+            y = 0f;
+            velocity = -velocity;
+        }
+
+
+        transform.position = new Vector3(transform.position.x, y, transform.position.z);
     }
 }
+
