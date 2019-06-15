1d1065da1ead7a42af20ed5d8830956dc641db71
JeongHo16 <31890732+JeongHo16@users.noreply.github.com>
2019-06-15 16:22:06 +0900

    update UI stage text

--- Assets/_Scripts/Windshield.cs
+++ Assets/_Scripts/Windshield.cs
@@ -5,12 +5,14 @@ using UnityEngine.UI;
 
 public class Windshield : MonoBehaviour
 {
-    public Slider tallerSlider;
-    public Text scoreText;
-    //public Text hpText;
+    public Slider stageTimer;
+    public Slider speedTimer;
+    public Slider tallerTimer;
+    public Text stageText;
+    public Text timeText;
     public GameObject trophys;
 
-    private int HP = 3;
+    //private int HP = 3;
 
     //public Sprite blackImage;
 
@@ -42,28 +44,28 @@ public class Windshield : MonoBehaviour
         float elaspedTime = 0f;
         float tallerTime = 3f;
 
-        tallerSlider.value = elaspedTime;
+        tallerTimer.value = elaspedTime;
         while (elaspedTime < tallerTime)
         {
             elaspedTime += Time.deltaTime;
-            tallerSlider.value += elaspedTime;
+            tallerTimer.value += elaspedTime;
             yield return null;
         }
 
         elaspedTime = 0f;
-        tallerSlider.value = 0f;
+        tallerTimer.value = 0f;
     }
 
     private void UpdateTrophy()
     {
         if (trophys.transform.childCount != 0)
-            scoreText.text = "<b>남은 보석 수: " + trophys.transform.childCount + "</b>";
+            stageText.text = "<b>Stage " + (5 - trophys.transform.childCount) + "</b>";
         else
-            scoreText.text = "<b>Game Clear</b>";
+            stageText.text = "<b>Game Clear</b>";
     }
 
-    public void AttackedByGhost()
-    {
-        HP -= 1;
-    }
+    //public void AttackedByGhost()
+    //{
+    //    HP -= 1;
+    //}
 }
