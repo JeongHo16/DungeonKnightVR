2fd736ddd37fdf37f906df2c1eff56687b7b7fa3
JeongHo16 <31890732+JeongHo16@users.noreply.github.com>
2019-06-15 23:51:32 +0900

    Up Down Trophy

--- Assets/_Scripts/Trophy.cs
+++ Assets/_Scripts/Trophy.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class Trophy : MonoBehaviour
 {
     float velocity = 1f;
-
+    float y;
     private void Update()
     {
         //transform.Rotate(new Vector3(0, 180, 0) * Time.deltaTime);
-        float y = transform.position.y;
+        y = transform.position.y;
         y += velocity * Time.deltaTime;
 
         if (y > 1f)
@@ -17,12 +17,12 @@ public class Trophy : MonoBehaviour
             y = 1f;
             velocity = -velocity;
         }
+
         if (y < 0f)
         {
             y = 0f;
             velocity = -velocity;
         }
-
 
         transform.position = new Vector3(transform.position.x, y, transform.position.z);
     }
